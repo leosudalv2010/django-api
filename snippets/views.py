43148d80e6736aabf9279ebbe64bf05295c78afe
liyiming <liyiming@andlinks.com>
2019-04-25 16:48:07 +0800

    ’integrate swagger

--- snippets/views.py
+++ snippets/views.py
@@ -1,17 +1,25 @@
-from rest_framework import generics, renderers
+from rest_framework import renderers, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
     """
-    List all the snippets or add a new snippet.
+    retrieve:
+        Return a snippet instance.
+    list:
+        Return all snippets,ordered by most recent joined.
+    create:
+        Create a new snippet.
+    delete:
+        Remove an existing snippet.
+    update:
+        Update an existing snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -20,42 +28,23 @@ class SnippetList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
-
-
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, pk):
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request):
+        """
+        Show the highlighted snippet.
+        """
         snippet = self.get_object()
         return Response(snippet.highlighted)
 
 
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    retrieve:
+        Return an user instance.
+    list:
+        Return all users, ordered by most recent joined.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
-
-
 
 
